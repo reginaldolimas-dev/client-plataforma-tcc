bc6f3f91651ed221a1e339614990f142b0117db5
reginaldo <reginaldolimas@gmail.com>
2026-07-13 20:03:49 -0300

    Update `aoPesquisar` to support filtering in `CurrencyPage`

--- src/pages/currency/CurrencyPage.jsx
+++ src/pages/currency/CurrencyPage.jsx
@@ -14,10 +14,10 @@ export function CurrencyPage() {
     aoPesquisar();
   }, []);
 
-  async function aoPesquisar() {
+  async function aoPesquisar(filtro) {
     try {
       setCarregando(true);
-      const resposta = await currencyService.listar();
+      const resposta = await currencyService.listar(filtro);
       setResultado(resposta);
     } catch (e) {
       console.error("Erro ao listar clientes", e);
